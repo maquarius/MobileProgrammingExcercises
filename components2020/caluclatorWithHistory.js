edc6cf0f3bf37be8fa1b0ba5afb031c0e2fd3879
Marius <mariusvanderbusse@hotmail.com>
2020-02-05 11:39:13 +0200

    fixed excercise 2 by removing curly braces from script. Made excercise 3 and 4, bind result to variable to make it render as expected

--- components2020/caluclatorWithHistory.js
+++ components2020/caluclatorWithHistory.js
@@ -10,27 +10,47 @@ import {
 import AwesomeButton from "react-native-really-awesome-button";
 
 export default function App() {
-  const [food, setFood] = useState("");
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+  const [text, setText] = useState("");
   const [history, setHistory] = useState([]);
 
-  const addFood = () => {
-    setFood(food);
-    setHistory([...history, { key: food }]);
-    setFood("");
+  const buttonPlus = () => {
+    let sum = number1 + number2;
+    setResult(sum);
+    let text = number1 + " + " + number2 + " = " + sum;
+    setText(text);
+    setHistory([...history, { key: text }]);
   };
 
-  const clearHistory = () => {
-    setFood("");
-    setHistory([]);
+  const buttonMinus = () => {
+    let sum = number1 - number2;
+    setResult(sum);
+    let text = number1 + " - " + number2 + " = " + sum;
+    setText(text);
+    setHistory([...history, { key: text }]);
   };
+
+  React.useEffect(() => {}, [history]);
 
   return (
     <View style={styles.container}>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>{result}</Text>
+      </View>
       <View style={styles.textinputContainer}>
         <TextInput
           style={styles.textBox}
-          onChangeText={input => setFood(input)}
-          value={food}
+          onChangeText={input => setNumber1(parseInt(input))}
+          value={number1}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.textBox}
+          onChangeText={input => setNumber2(parseInt(input))}
+          value={number2}
+          keyboardType="numeric"
         />
       </View>
       <View style={styles.buttonContainer}>
@@ -38,33 +58,24 @@ export default function App() {
           backgroundColor="pink"
           backgroundDarker="#e6acd2"
           textSize={40}
-          onPress={addFood}
+          onPress={buttonMinus}
         >
-          ADD
+          -
         </AwesomeButton>
         <AwesomeButton
           backgroundColor="pink"
           backgroundDarker="#e6acd2"
           textSize={40}
-          onPress={clearHistory}
+          onPress={buttonPlus}
         >
-          CLEAR
+          +
         </AwesomeButton>
       </View>
       <View style={styles.historyContainer}>
-        <Text style={{ fontSize: 30 }}>History: </Text>
-        <View
-          style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 1,
-            width: 300
-          }}
-        />
+        <Text>History: </Text>
         <FlatList
           data={history}
-          renderItem={({ item }) => (
-            <Text style={{ fontSize: 30 }}>{item.key}</Text>
-          )}
+          renderItem={({ item }) => <Text>{item.key}</Text>}
         />
       </View>
     </View>
@@ -87,13 +98,11 @@ const styles = StyleSheet.create({
   },
   historyContainer: {
     flex: 4,
-    alignItems: "center"
+    backgroundColor: "red"
   },
-
   textBox: {
     flex: 1,
-    width: 300,
-    fontSize: 30,
+    width: 200,
     borderBottomColor: "#77C3EC",
     borderWidth: 1,
     backgroundColor: "white",
@@ -101,8 +110,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: "row",
-    flex: 1,
-    justifyContent: "space-around"
+    flex: 1
   },
   result: {
     flex: 1
@@ -112,7 +120,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   button: {
-    padding: 80,
-    margin: 10
+    padding: 80
   }
 });
+s;
---
+++ components2020/guessingGame.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Button,
+  Text,
+  FlatList
+} from "react-native";
+import AwesomeButton from "react-native-really-awesome-button";
+
+export default function App() {
+  const [number, setNumber] = useState(0);
+  const [guess, setGuess] = useState(0);
+  const [result, setResult] = useState("Guess a number between 1-100");
+  const [tries, setTries] = useState(0);
+
+  const buttonGuess = () => {
+    if (number === guess) {
+      alert("You guessed the number in " + tries + " guesses");
+    } else if (guess > 100 || guess < 1) {
+      setResult(
+        "Your guess is not a valid guess. Please guess a number between 1-100"
+      );
+    } else if (number < guess) {
+      setResult("Your guess of " + guess + " is too high");
+      setTries(tries + 1);
+    } else if (number > guess) {
+      setResult("Your guess of " + guess + " is too low");
+      setTries(tries + 1);
+    }
+  };
+
+  React.useEffect(() => {
+    setNumber(Math.floor(Math.random() * 101));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.result}>
+        <Text>{number}</Text>
+        <Text style={styles.resultText}>{result}</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={styles.textBox}
+          onChangeText={input => setGuess(parseInt(input))}
+          value={guess}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <AwesomeButton
+          backgroundColor="pink"
+          backgroundDarker="#e6acd2"
+          textSize={40}
+          onPress={buttonGuess}
+        >
+          MAKE GUESS
+        </AwesomeButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#B8E2F2",
+    alignItems: "center"
+  },
+  textBox: {
+    width: 200,
+    borderBottomColor: "#77C3EC",
+    borderWidth: 1,
+    backgroundColor: "white",
+    margin: 4
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    flex: 3
+  },
+  result: {
+    flex: 2,
+    justifyContent: "center"
+  },
+  resultText: {
+    fontSize: 30
+  },
+  button: {
+    padding: 80
+  }
+});
