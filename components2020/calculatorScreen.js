5d2bc8ff8996b55e0ac53c208e4da0b9a7278ffb
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-03-03 22:53:27 +0200

    fixed git merge

--- components2020/calculatorScreen.js
+++ components2020/calculatorScreen.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-import React, {useState, version} from "react";
-import {
-  View,
-  Text,
-  Button,
-  FlatList,
-  TextInput,
-  Stylesheet
-} from "react-native";
-
-export default function calculatorScreen(props) {
-=======
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 
 export default function CalculatorScreen(props) {
   navigationOptions = { title: "Calculator" };
 
->>>>>>> 97aad9b4ef4f6b575fc764f0e9fff004a165d7fb
   const [number1, setNumber1] = useState("");
   const [number2, setNumber2] = useState("");
   const [total, setTotal] = useState(0);
@@ -41,7 +27,6 @@ export default function CalculatorScreen(props) {
 
   const { navigate } = props.navigation;
   return (
-<<<<<<< HEAD
     <View>
       <View>{total}</View>
       <TextInput
@@ -57,52 +42,15 @@ export default function CalculatorScreen(props) {
       <View>
         <Button onPress={() => buttonPlus()} title="+"></Button>
         <Button onPress={() => buttonMinus()} title="-"></Button>
-      
       </View>
       <Button
         onPress={() => navigate("history", { history: history })}
         title="History"
       />
-=======
-    <View style={styles.container}>
-      <Text style={styles.resultText}>{total}</Text>
-      <View stlye={styles.textInputContainer}>
-        <TextInput
-          onChangeText={input => setNumber1(input)}
-          value={number1}
-          keyboardType="numeric"
-          style={styles.textBox}
-        />
-        <TextInput
-          onChangeText={input => setNumber2(input)}
-          value={number2}
-          keyboardType="numeric"
-          style={styles.textBox}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          style={styles.button}
-          onPress={() => buttonMinus()}
-          title="-"
-        ></Button>
-        <Button
-          style={styles.button}
-          onPress={() => buttonPlus()}
-          title="+"
-        ></Button>
-        <Button
-          onPress={() => navigate("History", { data: { history } })}
-          title="History"
-        />
-      </View>
->>>>>>> 97aad9b4ef4f6b575fc764f0e9fff004a165d7fb
     </View>
   );
 }
 
-<<<<<<< HEAD
-=======
 CalculatorScreen.navigationOptions = ({ navigate }) => ({
   title: "Calculator"
 });
@@ -136,4 +84,3 @@ const styles = StyleSheet.create({
   },
   button: {}
 });
->>>>>>> 97aad9b4ef4f6b575fc764f0e9fff004a165d7fb
