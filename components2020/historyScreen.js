ddb9828477aad6c9f6fbcf8c318bf4d0c92cd66c
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-03-03 23:02:09 +0200

    ä

--- components2020/historyScreen.js
+++ components2020/historyScreen.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { View, Flatlist } from "react-native";
-<<<<<<< HEAD
-export default function historyScreen() {
-=======
 
 export default function HistoryScreen(props) {
   navigationOptions = { title: "History" };
->>>>>>> 97aad9b4ef4f6b575fc764f0e9fff004a165d7fb
   const { params } = props.navigation.state;
 
   return (
@@ -18,10 +14,6 @@ export default function HistoryScreen(props) {
     </View>
   );
 }
-<<<<<<< HEAD
-
-=======
 HistoryScreen.navigationOptions = ({ navigate }) => ({
   title: "History"
 });
->>>>>>> 97aad9b4ef4f6b575fc764f0e9fff004a165d7fb
